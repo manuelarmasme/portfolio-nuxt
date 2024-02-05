16d4639f1786f5e4308505d4a21fba286182c80f
manuelarmasme <manu12694@gmail.com>
2024-02-05 00:24:25 -0400

    layouts and tailwind

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,5 @@
 import { NuxtLink } from '../.nuxt/components';
 <template>
-  <nav>
-    <ul>
-      <li>
-        <NuxtLink to="/">Main</NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="/about">About</NuxtLink>
-      </li>
-    </ul>
-  </nav>
   <div>
     I'm index page
   </div>
