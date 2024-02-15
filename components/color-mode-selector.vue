652b6050af1ec5c5d0b582f9fbdb66fb22bfeaea
manuelarmasme <manu12694@gmail.com>
2024-02-14 23:04:19 -0400

    dark mode feature

---
+++ components/color-mode-selector.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="flex space-x-2 items-center">
+    <div class="text-gray-500 text-xs" v-if="showNextModelLabel"> Change to {{ nextMode }}</div>
+    <button
+      @mouseenter="showNextModelLabel = true"
+      @mouseleave="showNextModelLabel = false"
+      class="hover:bg-gray-200 dark:hover:bg-gray-600 px-2 py-1 text-gray-500"
+      @click="toggleMode"
+      >{{ nextModeIcon }}</button>
+  </div>
+</template>
+
+<script setup>
+const showNextModelLabel = ref(false)
+
+//setting colormode composable
+const colorMode = useColorMode()
+
+// mode options
+const modes = [
+  'system',
+  'light',
+  'dark'
+]
+
+//icon dark mode
+const nextModeIcons = {
+  system: '🌓',
+  light: '🌕',
+  dark: '🌑'
+}
+
+const nextMode = computed(()=>{
+  //get the index array option
+  const currentModeIndex = modes.indexOf(colorMode.preference)
+
+  //control de index
+  let nextModeIndex = null
+
+  if (currentModeIndex + 1 === modes.length) {
+    nextModeIndex = 0
+  } else {
+    nextModeIndex = currentModeIndex + 1
+  }
+
+  return modes[nextModeIndex]
+})
+
+//icon computed
+const nextModeIcon = computed(() => nextModeIcons[nextMode.value])
+
+//toggle function to change mode
+const toggleMode = () => colorMode.preference = nextMode.value
+
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,11 +2,15 @@ import { NuxtLink } from '../.nuxt/components';
 <template>
   <div class="container mx-auto max-w-2xl">
     <header class="flex justify-between items-center mt-5">
-      <div>
+      <div class="flex items-center space-x-12">
+        <div>
         <NuxtLink to="/" class="text-xl font-semibold p-2 hover:bg-gray-200">Manuel Armas</NuxtLink>
       </div>
 
       <Menu/>
+      </div>
+
+      <ColorModeSelector/>
     </header>
 
     <main class="p-2 mt-10">
@@ -24,3 +28,9 @@ useHead({
   titleTemplate: '%s - Manuel Armas',
 })
 </script>
+
+<style>
+body{
+  @apply bg-white dark:bg-gray-900 text-gray-700 dark:text-gray-300
+}
+</style>
