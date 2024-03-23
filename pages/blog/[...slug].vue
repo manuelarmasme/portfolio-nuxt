cdc487911509376e3be759df1d2f5e865f6f5212
manuelarmasme <manu12694@gmail.com>
2024-03-23 15:52:38 -0400

    404 page

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -2,22 +2,31 @@
   <article
     class="prose dark:prose-invert prose-pre:bg-white dark:prose-pre:bg-gray-800 prose-pre:text-gray-700 dark:prose-pre:text-gray-300 max-w-none"
   >
-    <ContentDoc v-slot="{ doc }">
-      <div class="grid grid-cols-6 gap-16">
-        <div
-          :class="{ 'col-span-4': doc.toc, 'col-span-6': !doc.toc }"
-        >
-          <ContentRenderer :value="doc" />
+    <ContentDoc >
+      <template #not-found>
+        <h1>Document not Found</h1>
+        <p>This blogspot could not be found</p>
+      </template>
+
+      <template v-slot="{ doc }">
+        <div class="grid grid-cols-6 gap-16">
+          <div
+            :class="{ 'col-span-4': doc.toc, 'col-span-6': !doc.toc }"
+          >
+            <ContentRenderer :value="doc" />
+          </div>
+          <div class="col-span-2 not-prose" v-if="doc.toc">
+            <aside class="sticky top-8">
+              <div class="font-semibold mb-2">Table of content</div>
+              <nav>
+                <TocLinks :links="doc.body.toc.links" :active-id="activeId"/>
+              </nav>
+            </aside>
+          </div>
         </div>
-        <div class="col-span-2 not-prose" v-if="doc.toc">
-          <aside class="sticky top-8">
-            <div class="font-semibold mb-2">Table of content</div>
-            <nav>
-              <TocLinks :links="doc.body.toc.links" :active-id="activeId"/>
-            </nav>
-          </aside>
-        </div>
-      </div>
+
+      </template>
+
     </ContentDoc>
   </article>
 </template>
